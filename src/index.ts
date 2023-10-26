16b7efdea9af3a7b58181807ed3544404c3e5436
Sinar Pandora <SinarPandora@outlook.com>
2023-10-26 18:46:42 +0800

    Add cors config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import {Hono} from "hono";
+import {cors} from "hono/cors";
 import {api as articleApi} from "./article";
 
 const app = new Hono().basePath('/api');
 
-app.get('/', (c) => c.json({ message: 'Hello Hono!' }));
+app.use(
+    '*',
+    cors({
+        origin: ['http://localhost:21300', 'https://splat.yohane.cc', 'https://spla.tools'],
+        allowMethods: ['PUT', 'POST', 'GET', 'OPTIONS'],
+        maxAge: 600,
+        credentials: true,
+    })
+)
+
+app.get('/', (c) => c.json({message: 'Hello Hono!'}));
 app.route('/article', articleApi);
 
 export default app;
