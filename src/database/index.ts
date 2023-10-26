846c7961524ca0bf449ad0f340db20761f3152fc
sdong <sinar.shihui.dong@kingland.com>
2023-10-26 14:50:55 +0800

    Mark as prisma client

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,6 +4,6 @@ import {withAccelerate} from "@prisma/extension-accelerate";
 
 const prisma = (process.env.VERCEL === '1'
     ? new PrismaEdge().$extends(withAccelerate())
-    : new PrismaClient())
+    : new PrismaClient()) as unknown as PrismaClient
 
 export default prisma;
